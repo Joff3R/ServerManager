86d8cb37668b3d314296054253588d191eb847de
Bartosz <bkutkowski@gmail.com>
2022-01-16 18:13:52 +0100

    Interfaces to support server added

---
+++ src/main/java/com/kutkowski/server/repo/ServerRepo.java
@@ -0,0 +1,8 @@
+package com.kutkowski.server.repo;
+
+import com.kutkowski.server.model.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServerRepo extends JpaRepository<Server, Long> {
+    Server findByIpAddress(String ipAddress);
+}
