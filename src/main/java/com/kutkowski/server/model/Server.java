bda51785b601fa015d6cde511f8141f9d2bf5241
Bartosz <bkutkowski@gmail.com>
2022-01-16 17:51:14 +0100

    Status model added with enum

---
+++ src/main/java/com/kutkowski/server/model/Server.java
@@ -0,0 +1,13 @@
+package com.kutkowski.server.model;
+
+import com.kutkowski.server.enumeration.Status;
+
+public class Server {
+    private long id;
+    private String ipAddress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imageUrl;
+    private Status status;
+}
