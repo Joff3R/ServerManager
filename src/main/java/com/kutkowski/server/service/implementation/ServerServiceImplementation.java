ebc2e5f5d90db36f38a5a31f2bff0a49196d8dab
Bartosz <bkutkowski@gmail.com>
2022-01-16 22:07:57 +0100

    ServerServiceImplementation - finished implementing methods

--- src/main/java/com/kutkowski/server/service/implementation/ServerServiceImplementation.java
+++ src/main/java/com/kutkowski/server/service/implementation/ServerServiceImplementation.java
@@ -5,12 +5,15 @@ import com.kutkowski.server.repo.ServerRepo;
 import com.kutkowski.server.service.ServerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.transaction.Transactional;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.util.Collection;
+import java.util.Random;
 
 import static com.kutkowski.server.enumeration.Status.SERVER_DOWN;
 import static com.kutkowski.server.enumeration.Status.SERVER_UP;
@@ -41,25 +44,31 @@ public class ServerServiceImplementation implements ServerService {
 
     @Override
     public Collection<Server> list(int limit) {
-        return null;
+        log.info("Fetching all servers");
+        return serverRepo.findAll(PageRequest.of(0, limit)).toList();
     }
 
     @Override
     public Server get(Long id) {
-        return null;
+        log.info("Fetching server by id: {}", id);
+        return serverRepo.findById(id).get();
     }
 
     @Override
     public Server update(Server server) {
-        return null;
+        log.info("Updating serve: {}", server.getName());
+        return serverRepo.save(server);
     }
 
     @Override
     public Boolean delete(Long id) {
-        return null;
+        log.info("Deleting server by id: {}", id);
+        serverRepo.deleteById(id);
+        return true;
     }
 
     private String setServerImageUrl() {
-        return null;
+        String[] imageNames = {"server1.png", "server2.png", "server3.png", "server4.png"};
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/image" + imageNames[new Random().nextInt(4)]).toUriString();
     }
 }
