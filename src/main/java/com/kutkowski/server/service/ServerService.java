8341684bc38d2e71a4b92a9384fde2d54d80c2d2
Bartosz <bkutkowski@gmail.com>
2022-01-16 21:12:05 +0100

    ServerServiceImplementation added methods: create, ping

--- src/main/java/com/kutkowski/server/service/ServerService.java
+++ src/main/java/com/kutkowski/server/service/ServerService.java
@@ -2,12 +2,13 @@ package com.kutkowski.server.service;
 
 import com.kutkowski.server.model.Server;
 
+import java.io.IOException;
 import java.util.Collection;
 
 public interface ServerService {
     Server create(Server server);
 
-    Server ping(String ipAddress);
+    Server ping(String ipAddress) throws IOException;
 
     Collection<Server> list(int limit);
 
--- src/main/java/com/kutkowski/server/service/implementation/ServerServiceImplementation.java
+++ src/main/java/com/kutkowski/server/service/implementation/ServerServiceImplementation.java
@@ -8,7 +8,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Collection;
+
+import static com.kutkowski.server.enumeration.Status.SERVER_DOWN;
+import static com.kutkowski.server.enumeration.Status.SERVER_UP;
 
 @RequiredArgsConstructor
 @Service
@@ -19,12 +24,19 @@ public class ServerServiceImplementation implements ServerService {
 
     @Override
     public Server create(Server server) {
-        return null;
+        log.info("Saving new server: {}", server.getName());
+        server.setImageUrl(setServerImageUrl());
+        return serverRepo.save(server);
     }
 
     @Override
-    public Server ping(String ipAddress) {
-        return null;
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Pinging server IP: {}", ipAddress);
+        var server = serverRepo.findByIpAddress(ipAddress);
+        var address = InetAddress.getByName(ipAddress);
+        server.setStatus(address.isReachable(10000) ? SERVER_UP : SERVER_DOWN);
+        serverRepo.save(server);
+        return server;
     }
 
     @Override
@@ -44,6 +56,10 @@ public class ServerServiceImplementation implements ServerService {
 
     @Override
     public Boolean delete(Long id) {
+        return null;
+    }
+
+    private String setServerImageUrl() {
         return null;
     }
 }
