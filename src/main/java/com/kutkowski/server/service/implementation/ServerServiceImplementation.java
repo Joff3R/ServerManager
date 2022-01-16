fa455f5d27ebebefc4b13484de94885186e009e1
Bartosz <bkutkowski@gmail.com>
2022-01-16 21:04:45 +0100

    ServerServiceImplementation annotations finished

--- src/main/java/com/kutkowski/server/service/implementation/ServerServiceImplementation.java
+++ src/main/java/com/kutkowski/server/service/implementation/ServerServiceImplementation.java
@@ -1,11 +1,21 @@
 package com.kutkowski.server.service.implementation;
 
 import com.kutkowski.server.model.Server;
+import com.kutkowski.server.repo.ServerRepo;
 import com.kutkowski.server.service.ServerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.Collection;
 
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
 public class ServerServiceImplementation implements ServerService {
+    private final ServerRepo serverRepo;
 
     @Override
     public Server create(Server server) {
